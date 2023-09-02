6e0e3cf4311f5457d5ad63d7ba833cfe58030a0d
Safayat-Hasan <safayathasananim20@gmail.com>
2023-09-02 16:52:22 +0600

    added blog

--- js/ph.js
+++ js/ph.js
@@ -88,76 +88,9 @@ function displayOops() {
     oopsContainer.appendChild(oopsPic);
 }
 
-function sortViews() {
-    const loadVideoAll = async () => {
-        const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
-        const data = await res.json();
-        console.log(data.data);
-        const videos = data.data;
-
-        displayVideosAll(videos);
-        videos.forEach(video => {
-            const views = video.others.views;
-            const viewsNum = parseInt(views);
-            // console.log(viewsNum);
-            video.others.views = viewsNum;
-        })
-        // videos.sort(viewsNum);
-    }
-
-    const displayVideosAll = (videos) => {
-        const videoContainer = document.getElementById('video-container');
-        videoContainer.textContent = '';
-        const oopsContainer = document.getElementById('oops-section');
-        oopsContainer.textContent = '';
-        videos.forEach(video => {
-            const videoCard = document.createElement('div');
-            videoCard.classList = `card bg-gray-100 p-4 shadow-xl`
-            const views = video.others.views;
-            const viewsNum = parseInt(views);
-            console.log(viewsNum);
-            if (video.authors[0].verified === true) {
-                videoCard.innerHTML = `
-                <figure><img src="${video.thumbnail}" alt="" /></figure>
-                <div class="flex gap-3">
-                    <div>
-                        <img class="mt-3 w-10 h-10 rounded-[40px]" src="${video.authors[0].profile_picture}" alt="" /></figure>
-                    </div>
-                    <div class="mt-4">
-                        <h2 class="font-bold text-base">${video.title}</h2>
-                        <div class="flex gap-2">
-                            <p class="font-normal text-sm text-[#171717B3]">${video.authors[0].profile_name}</p>
-                            <img src="verified.png" alt="" />
-                        </div>    
-                        <p id="views" class="font-normal text-sm text-[#171717B3]">${video.others.views} views</p>
-                    </div>
-                </div>
-            `
-            }
-            else {
-                videoCard.innerHTML = `
-                <figure><img src="${video.thumbnail}" alt="" /></figure>
-                <div class="flex gap-3">
-                    <div>
-                        <img class="mt-3 w-10 h-10 rounded-[40px]" src="${video.authors[0].profile_picture}" alt="" /></figure>
-                    </div>
-                    <div class="mt-4">
-                        <h2 class="font-bold text-base">${video.title}</h2>
-                        <p class="font-normal text-sm text-[#171717B3]">${video.authors[0].profile_name}</p>
-                        <p id="views" class="font-normal text-sm text-[#171717B3]">${video.others.views} views</p>
-                    </div>
-                </div>
-            `
-            }
-            videoContainer.appendChild(videoCard);
-
-        });
-    }
-    loadVideoAll();
-}
 
 // loadVideo(1000);
-function sortViews2() {
+function sortViews() {
     // Get the container of the cards
     const cardContainer = document.getElementById("video-container");
 
@@ -174,5 +107,8 @@ function sortViews2() {
     // cardContainer.textContent = '';
     // Reappend the sorted cards to the container
     cards.forEach(card => cardContainer.appendChild(card));
+}
 
+function blogSite(){
+    window.location.href='blog.html';
 }
